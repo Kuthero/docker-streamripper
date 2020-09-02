e8e0f931a51bd2d26535fdfad12b0d56cd34fcdc
Kuthero <42418700+Kuthero@users.noreply.github.com>
2020-09-02 12:48:41 -0400

    init

--- run.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-if [ $# -gt 0 ]; then
-  # append relay config if any parameters are given
-  streamripper "$@" -r 8000 -R 0
-else
-  # otherwise, default to no args (=> help output)
-  streamripper
-fi
